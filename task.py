cd80fa6f7a6bd92038b195e377cb6e60b3e7eb67
Joao Goncalo Rodrigues Fernandes Couto <JOGNC@vestas.com>
2021-10-16 15:44:44 +0100

    Dependencies: start_date | end_date rules added;\n Dependencies: childs or parent cannot be dependencies; \n Childs: start_Date | end_date | duration rules added; \n

--- task.py
+++ task.py
@@ -23,11 +23,11 @@ class Task():
     responsible: int
     parent: int
     structure_level:int
-    dependencies: list
+    _dependencies: list
     task_id:int
     
     
-    def __init__(self,name,start_date,duration,description="",cost=(0,"EUR"),parent=[],childs=[]):
+    def __init__(self,name,start_date,duration,description="",cost=(0,"EUR"),structure_level=0,parent=[],childs=[],dependencies=[]):
         self.name=name
         self.start_date=start_date
         self.duration=duration
@@ -36,10 +36,20 @@ class Task():
         self.task_id=id(self)
         self.parent=parent
         self.childs=childs
+        self.dependencies=dependencies
+        self.structure_level=structure_level
+        
     
     @property
     def end_date(self):
+        #analyze subtasks:
+        if self.childs:
+            self.end_date=self.childs[0].end_date
+            for child in self.childs:
+                if child.end_date>self.end_date:
+                    self.end_date=child.end_date
         return self._end_date
+    
     @end_date.setter
     def end_date(self,end_date):
         if end_date < self.start_date+self.duration:
@@ -56,10 +66,32 @@ class Task():
     
     @property
     def start_date(self):
+        #analyze dependencies:
+        for dependency in self.dependencies:
+            if dependency.end_date>self.start_date:
+                self.start_date=dependency.end_date
+        #analyze subtasks:
+        if self.childs:
+            self.start_date=self.childs[0].start_date
+            for child in self.childs:
+                if child.start_date<self.start_date:
+                    self.start_date=child.start_date
         return self._start_date
+    
     @start_date.setter
     def start_date(self,start_date):
         self._start_date=start_date
+
+    @property
+    def dependencies(self):
+        return self._dependencies
+    @dependencies.setter
+    def dependencies(self,dependencies):
+        if any(task in dependencies for task in self.childs):
+            raise AttributeError("A subtask of a task cannot be a dependency of the same task")
+        if self.parent in dependencies:
+            raise AttributeError("The parent of a task cannot be a dependency of the that task")
+        self._dependencies=dependencies
 
     def check_end_date(self):
         if self.end_date < self.start_date+self.duration:
@@ -79,20 +111,32 @@ class Task():
     def isGroup(self,Subtasks):
         eSubtasks=[]
         self.childs=[]
+        self.duration=timedelta()
         for task in Subtasks:
             task.parent=self
             task.structure_level=self.structure_level-1
             eSubtasks.append(task)
             self.childs.append(task)
+            self.duration+=childs.duration
         return eSubtasks
+
+    def addDependencies(self,dependencies):
+        for task in dependencies:
+            self.dependencies.append(task)
+        
+            
+            
             
     
-
-
-    #method to promote/demote task level (relation with parent) -> Missing recalculation of start,end,duration for Task with childs
-    #method to check if added dependencies are in agreement with the level/parent
+    # if there are dependencies, the class is automatically changing the start_date if it is smaller than the max(end_date) of the dependencies -> does it make sense?
+    #method to promote/demote task level (relation with parent) -> Missing validation
+    #method to check if added dependencies are in agreement with the level/parent -> Missing validation
     #method to validate currency
-    
+    # dunder methods missing:
+        # __eq__ and similar
+        # __lt__ __gt__ and similar
+        # __repr__
+        # __str__
     
 
 
